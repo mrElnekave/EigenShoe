4566a3e73a351d0cee5e5a353d8287bdf136a06b
Yamm Elnekave <klavlavigugu@gmail.com>
2023-03-21 21:04:38 -0700

    only on left shoes

--- REI/LoadImages.m
+++ REI/LoadImages.m
@@ -1,4 +1,4 @@
-function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, pics_per_shoe)
+function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, pics_per_shoe, show)
     % LoadImages - loads and scales images
     %   Gives you access to 4 important variables, describing the shoe array.
     %   Taken from the train directory.
@@ -6,6 +6,7 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
     %   Syntax:
     %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages( names );
     %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages( names , 3);
+    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages( names , 3, true);
     %
     %   Description:
     %       LoadImages() - loads and scales images
@@ -14,6 +15,7 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
     %       names - Names of the files at Train/name, there should be files of
     %       Train/name1/2/3/4. ie. 4 per name.
     %       pics_per_shoe - Number of images per shoe, default is 4.
+    %       show - Show the images as they are loaded, default is false.
     %
     %   Output:
     %       IMAGE_DIM - The row and column size of a resized image
@@ -36,6 +38,10 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
         pics_per_shoe = 4;
     end
 
+    if ~exist('show', 'var')
+        show = false;
+    end
+
     %% Define sizes
     IMAGE_DIM = 255;
     IMAGE_SIZE = IMAGE_DIM * IMAGE_DIM;
@@ -50,7 +56,6 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
 
     for name_i = 1:length(names)
         name = names(name_i);
-        disp(name)
         files = dir("Train/" + name + "*.jpg");
         num_this_shoe = numel(files); % Should be 4
 
@@ -65,16 +70,21 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
             fprintf(1, 'Now reading %s\n', fullFileName);
 
             imageArray = imread(fullFileName);
-            imshow(imageArray);
+
+            if show
+                imshow(imageArray);
+            end
 
             imageArray = im2gray(imageArray);
             imageArray = imresize(imageArray, [IMAGE_DIM, IMAGE_DIM]);
             index = (name_i - 1) * pics_per_shoe + i;
             Shoes(:, index) = reshape(double(im2gray(imageArray)), IMAGE_SIZE, 1);
             Lookup(index) = name;
-            disp(index)
 
-            pause(.2);
+            if show
+                pause(.2);
+            end
+
         end
 
         close
@@ -82,5 +92,19 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImages(names, p
         clear filename files fullFileName i imageArray
 
     end
+
+    % % Get only the right facing shoes ie numbers 1,2 but not 3,4 and 5,6 but not 7,8
+    % newShoes = zeros(IMAGE_SIZE, num_shoes / 2);
+
+    % for i = 1:num_shoes
+    %     good = mod(floor((i - 1) / 2), 2) == 0;
+
+    %     if good
+    %         newShoes(:, floor((i - 1) / 2) + 1) = Shoes(:, i);
+    %     end
+
+    % end
+
+    % Shoes = newShoes;
 
 end
