e1ce8459f8c8776e8af3f2ffa52275231265c645
Yamm Elnekave <klavlavigugu@gmail.com>
2023-03-21 22:40:54 -0700

    Construct model for big shoe data

--- REI/LoadImagesBig.m
+++ REI/LoadImagesBig.m
@@ -1,29 +1,43 @@
-function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig(names, pics_per_shoe, show)
+function [IMAGE_DIM, IMAGE_SIZE, num_shoes, LeftShoes, RightShoes, Lookup] = LoadImagesBig(pics_per_shoe, show)
     % LoadImages - loads and scales images
     %   Gives you access to 4 important variables, describing the shoe array.
     %   Taken from the train directory.
     %
+    %   File Structure:
+    %       Left/
+    %           NAME/
+    %               *.jpg
+    %               ...
+    %           NAME2/ ...
+    %       Right/
+    %           NAME/
+    %               *.jpg
+    %               ...
+    %           NAME2/ ...
+    %
     %   Syntax:
-    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig( names );
-    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig( names , 3);
-    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig( names , 3, true);
+    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, LeftShoes, RightShoes, Lookup] = LoadImagesBig( );
+    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, LeftShoes, RightShoes, Lookup] = LoadImagesBig( 3 );
+    %       [IMAGE_DIM, IMAGE_SIZE, num_shoes, LeftShoes, RightShoes, Lookup] = LoadImagesBig( 3, true );
     %
     %   Description:
     %       LoadImages() - loads and scales images
     %
     %   Inputs:
-    %       names - Names of the files at Train/name, there should be files of
-    %       Train/name1/2/3/4. ie. 4 per name.
     %       pics_per_shoe - Number of images per shoe, default is 4.
     %       show - Show the images as they are loaded, default is false.
     %
     %   Output:
     %       IMAGE_DIM - The row and column size of a resized image
     %       IMAGE_SIZE - IMAGE_DIM * IMAGE_DIM, the size of an image vector
-    %       num_shoes - Number of files (images parsed)
-    %       Shoes - Matrix of vectors of IMAGE_SIZE images.
+    %       num_shoes - Number of different shoes (images parsed / 2)
+    %       LeftShoes - A Matrix of all the left shoes, each column is a
+    %       reshaped image
+    %       RightShoes - A Matrix of all the right shoes, each column is a
+    %       reshaped image
     %       Lookup - A Matrix with the name each index of Shoe is associated
-    %       with
+    %       with. The first column is the left shoe, the second column is the
+    %       right shoe.
     %
     %
     %   See also: RunEigenShoe.mlx
@@ -46,28 +60,32 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig(names
     IMAGE_DIM = 255;
     IMAGE_SIZE = IMAGE_DIM * IMAGE_DIM;
 
-    %% Load data from folder using Train directory
-
-    files = dir('Train/*.jpg');
+    % Left
+    files = dir('Left/*/*');
     num_shoes = numel(files);
 
-    Shoes = zeros(IMAGE_SIZE, num_shoes);
-    Lookup = string(zeros(num_shoes, 1));
+    % lookup for both left and right shoes
+    Lookup = string(zeros(num_shoes, 2));
 
-    for name_i = 1:length(names)
-        name = names(name_i);
-        files = dir("Train/" + name + "*.jpg");
-        num_this_shoe = numel(files); % Should be 4
+    LeftShoes = zeros(IMAGE_SIZE, num_shoes);
+
+    left_folders = dir('Left/*');
+
+    for i = 3:numel(left_folders)
+        left_files = dir("Left/" + left_folders(i).name + "/*.jpg");
+        num_this_shoe = numel(left_files); % Should be 4
 
         if num_this_shoe ~= pics_per_shoe
             error("There should be " + pics_per_shoe + " images per shoe, but there are " + num_this_shoe + " for " + name);
         end
 
-        for i = 1:num_this_shoe
-            filename = files(i).name;
+        for j = 1:num_this_shoe
+            filename = left_files(j).name;
 
-            fullFileName = fullfile(files(i).folder, filename);
-            fprintf(1, 'Now reading %s\n', fullFileName);
+            fullFileName = fullfile(left_files(j).folder, filename);
+            index = (i - 1) * pics_per_shoe + j;
+
+            fprintf(1, 'Now reading %s for left shoe %d of %d \r', fullFileName, index, num_shoes);
 
             imageArray = imread(fullFileName);
 
@@ -77,9 +95,10 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig(names
 
             imageArray = im2gray(imageArray);
             imageArray = imresize(imageArray, [IMAGE_DIM, IMAGE_DIM]);
-            index = (name_i - 1) * pics_per_shoe + i;
-            Shoes(:, index) = reshape(double(im2gray(imageArray)), IMAGE_SIZE, 1);
-            Lookup(index) = name;
+            LeftShoes(:, index) = reshape(double(im2gray(imageArray)), IMAGE_SIZE, 1);
+
+            % Lookup
+            Lookup(index, 1) = filename(1:3);
 
             if show
                 pause(.2);
@@ -87,9 +106,55 @@ function [IMAGE_DIM, IMAGE_SIZE, num_shoes, Shoes, Lookup] = LoadImagesBig(names
 
         end
 
-        close
+        clear filename left_files fullFileName j imageArray num_this_shoe ans files
 
-        clear filename files fullFileName i imageArray
+    end
+
+    % Right
+
+    files = dir('Right/*/*');
+    num_shoes = numel(files);
+
+    RightShoes = zeros(IMAGE_SIZE, num_shoes);
+
+    right_folders = dir('Right/*');
+
+    for i = 3:numel(right_folders)
+        right_files = dir("Right/" + right_folders(i).name + "/*.jpg");
+        num_this_shoe = numel(right_files); % Should be 4
+
+        if num_this_shoe ~= pics_per_shoe
+            error("There should be " + pics_per_shoe + " images per shoe, but there are " + num_this_shoe + " for " + name);
+        end
+
+        for j = 1:num_this_shoe
+            filename = right_files(j).name;
+
+            fullFileName = fullfile(right_files(j).folder, filename);
+            index = (i - 1) * pics_per_shoe + j;
+
+            fprintf(1, 'Now reading %s for right shoe %d of %d \r', fullFileName, index, num_shoes);
+
+            imageArray = imread(fullFileName);
+
+            if show
+                imshow(imageArray);
+            end
+
+            imageArray = im2gray(imageArray);
+            imageArray = imresize(imageArray, [IMAGE_DIM, IMAGE_DIM]);
+            RightShoes(:, index) = reshape(double(im2gray(imageArray)), IMAGE_SIZE, 1);
+
+            % Lookup
+            Lookup(index, 2) = filename(1:3);
+
+            if show
+                pause(.2);
+            end
+
+        end
+
+        clear filename right_files fullFileName j imageArray num_this_shoe ans files
 
     end
 
